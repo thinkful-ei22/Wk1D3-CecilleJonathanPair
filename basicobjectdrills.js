ce9f12206b02444b4fa69470769c2b2138d86114
Jonathan Kim <jonkim204@gmail.com>
2018-06-13 09:13:19 -0700

    delete keys drill

--- basicobjectdrills.js
+++ basicobjectdrills.js
@@ -33,3 +33,17 @@ function personMaker() {
   };
   return person;
 }
+
+
+const sampleObj = {
+  foo: 'foo',
+  bar: 'bar',
+  bizz: 'bizz',
+  bang: 'bang',
+};
+
+function keyDeleter(obj) {
+  delete obj.foo;
+  delete obj.bar;
+  return obj;
+}
