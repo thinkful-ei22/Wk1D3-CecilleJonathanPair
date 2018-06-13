3bae9694231624e7c76aec1b756ccada837eb410
Jonathan Kim <jonkim204@gmail.com>
2018-06-13 09:35:21 -0700

    validate object keys

--- basicobjectdrills.js
+++ basicobjectdrills.js
@@ -85,3 +85,34 @@ const scratchData = [
 function findById(items, idNum) {
   return items.find(item => item.id === idNum);
 }
+
+// running the function with `objectA` and `expectedKeys`
+// should return `true`
+const objectA = {
+  id: 2,
+  name: 'Jane Doe',
+  age: 34,
+  city: 'Chicago',
+};
+
+// running the function with `objectB` and `expectedKeys`
+// should return `false`
+const objectB = {
+  id: 3,
+  age: 33,
+  city: 'Peoria',
+};
+
+const expectedKeys = ['id', 'name', 'age', 'city'];
+
+function validateKeys(object, expectedKeys) {
+  if (Object.keys(object).length !== expectedKeys.length) {
+    return false;
+  }
+  for (let i = 0; i< expectedKeys.length; i++) {
+    if (!Object.keys(object).find(key => key === expectedKeys[i])) {
+      return false;
+    }
+  }
+  return true;
+}
