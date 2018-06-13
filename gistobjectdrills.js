9a41cd86e0543af2d72bd3deacb2a04c2993cecc
Cecille Salazar <cecillersalazar@gmail.com>
2018-06-13 14:11:03 -0700

    Finished drill 7

--- gistobjectdrills.js
+++ gistobjectdrills.js
@@ -54,40 +54,40 @@ Use console.log to show each property name and its associated value.
 //   }
 // }
 
-//Redo your Cracking the Code problem from String Drills but this time use an object as your cipher. Additionally, create a 
+//Redo your Cracking the Code problem from String Drills but this time use an object as your cipher. Additionally, create a
 //decodeWords function that utilizes your decode function to accept a single string of words, and then return the fully decoded message as a string.
 
-let cipher = {
-  a: 2,
-  b: 3,
-  c: 4,
-  d: 5,
-};
-
-function decode (word) {
-  let strArray = word.split(' ');
-  let returnStr = '';
-  for (let i = 0; i < strArray.length; i++) {
-    if (strArray[i][0] === 'a') {
-      returnStr += strArray[i][cipher.a - 1];
-    }
-    else if (strArray[i][0] === 'b') {
-      returnStr += strArray[i][cipher.b - 1];
-    }
-    else if (strArray[i][0] === 'c') {
-      returnStr += strArray[i][cipher.c - 1];
-    }
-    else if (strArray[i][0] === 'd') {
-      returnStr += strArray[i][cipher.d - 1];
-    }
-    else {
-      returnStr += ' ';
-    }
-  }
-  return returnStr;
-}
-
-console.log(decode('craft block argon meter bells brown croon droop'));
+// let cipher = {
+//   a: 2,
+//   b: 3,
+//   c: 4,
+//   d: 5,
+// };
+//
+// function decode (word) {
+//   let strArray = word.split(' ');
+//   let returnStr = '';
+//   for (let i = 0; i < strArray.length; i++) {
+//     if (strArray[i][0] === 'a') {
+//       returnStr += strArray[i][cipher.a - 1];
+//     }
+//     else if (strArray[i][0] === 'b') {
+//       returnStr += strArray[i][cipher.b - 1];
+//     }
+//     else if (strArray[i][0] === 'c') {
+//       returnStr += strArray[i][cipher.c - 1];
+//     }
+//     else if (strArray[i][0] === 'd') {
+//       returnStr += strArray[i][cipher.d - 1];
+//     }
+//     else {
+//       returnStr += ' ';
+//     }
+//   }
+//   return returnStr;
+// }
+//
+// console.log(decode('craft block argon meter bells brown croon droop'));
 // function decode (word) {
 //   let keys = Object.keys(cipher);
 //   let returnString = "";
@@ -108,3 +108,40 @@ console.log(decode('craft block argon meter bells brown croon droop'));
 // };
 
 //  console.log(decode('craft block argon meter bells brown croon droop'));
+
+function LOTR (name, nickname, race, origin, attack, defense) {
+  return {
+    name, nickname, race, origin, attack, defense,
+    describe: function() {
+      console.log(`${this.name} is a ${this.race} from ${this.origin}`);
+    },
+    evaluateFight: function(character) {
+      if (this.attack - this.defense > 0) {
+        let x = this.attack - this.defense;
+      } else if (this.defense > this.attack) {
+        let y = 0;
+      } else if (this.defense < this.attack) {
+        y = this.attack - this.defense;
+      }
+      return `Your opponent takes ${x} damage and you receive ${y} damage`;
+    }
+  }
+};
+
+const characters = [];
+
+const Gandalf = LOTR('Gandalf the White', 'gandalf', 'Wizard', 'Middle Earth', 10, 6);
+const Bilbo = LOTR('Bilbo Baggins', 'bilbo', 'Hobbit', 'The Shire', 2, 1);
+const Frodo = LOTR('Frodo Baggins', 'frodo', 'Hobbit', 'The Shire', 3, 2);
+const Aragorn = LOTR('Aragorn son of Arathorn', 'aragorn', 'Man', 'Dunnedain', 6, 8);
+const Legolas = LOTR('Legolas', 'legolas', 'Elf', 'Woodland Realm', 8, 5);
+const Gimli = LOTR('Gimli', 'gimli', 'Dwarf', 'Mines', 7, 5);
+
+characters.push(Gandalf, Bilbo, Frodo, Aragorn, Legolas, Gimli);
+//console.log(characters);
+
+//characters.find(character => character.nickname === 'aragorn').describe();
+
+//console.log(characters.filter(character => character.race === 'Hobbit'));
+
+console.log(characters.filter(character => character.attack > 5));
