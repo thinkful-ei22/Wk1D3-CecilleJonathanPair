4ae4b6fa557c984279ccfc849bac0a48a3c24d1c
Jonathan Kim <jonkim204@gmail.com>
2018-06-13 13:15:32 -0700

    drill 6

--- gistobjectdrills.js
+++ gistobjectdrills.js
@@ -1,4 +1,4 @@
-'use strict'
+'use strict';
 // const loaf = {
 //   flour: 300,
 //   water: 210,
@@ -38,18 +38,73 @@ Use console.log to show each property name and its associated value.
 // Store these objects in an array.
 // Iterate over the array and use console.log to show each person's job title and name.
 
-const workers = [
-  {name: 'Jon', job_title: "superhero"},
-  {name: 'Bob', job_title: 'builder', boss: 'Jon'},
-  {name: 'Jane', job_title: 'Chef', boss: 'Jon'},
-];
+// const workers = [
+//   {name: 'Jon', jobTitle: "superhero"},
+//   {name: 'Bob', jobTitle: 'builder', boss: 'Jon'},
+//   {name: 'Jane', jobTitle: 'Chef', boss: 'Jon'},
+// ];
 
-for (let i = 0; i < workers.length; i++) {
-  const person = workers[i];
-  if (!person.boss) {
-    console.log(`${person.name} ${person.job_title} doesn't report to anybody`)
+// for (let i = 0; i < workers.length; i++) {
+//   const person = workers[i];
+//   if (!person.boss) {
+//     console.log(`${person.name} ${person.jobTitle} doesn't report to anybody`)
+//   }
+//   if (person.boss) {
+//     console.log(`${person.name} ${person.jobTitle} reports to ${person.boss}.`);
+//   }
+// }
+
+//Redo your Cracking the Code problem from String Drills but this time use an object as your cipher. Additionally, create a 
+//decodeWords function that utilizes your decode function to accept a single string of words, and then return the fully decoded message as a string.
+
+let cipher = {
+  a: 2,
+  b: 3,
+  c: 4,
+  d: 5,
+};
+
+function decode (word) {
+  let strArray = word.split(' ');
+  let returnStr = '';
+  for (let i = 0; i < strArray.length; i++) {
+    if (strArray[i][0] === 'a') {
+      returnStr += strArray[i][cipher.a - 1];
+    }
+    else if (strArray[i][0] === 'b') {
+      returnStr += strArray[i][cipher.b - 1];
+    }
+    else if (strArray[i][0] === 'c') {
+      returnStr += strArray[i][cipher.c - 1];
+    }
+    else if (strArray[i][0] === 'd') {
+      returnStr += strArray[i][cipher.d - 1];
+    }
+    else {
+      returnStr += ' ';
+    }
   }
-  if (person.boss) {
-    console.log(`${person.name} ${person.job_title} reports to ${person.boss}.`);
-  }
+  return returnStr;
 }
+
+console.log(decode('craft block argon meter bells brown croon droop'));
+// function decode (word) {
+//   let keys = Object.keys(cipher);
+//   let returnString = "";
+//   let strArray = word.split(" ");
+//   switch(word[0]) {
+//   case keys[0]:
+//     returnString += word[cipher.a - 1];
+//   case keys[1]:
+//     returnString += word[cipher.b - 1];
+//   case keys[2]:
+//     returnString += word[cipher.c - 1];
+//   case keys[3]:
+//     returnString += word[cipher.d - 1];
+//   default:
+//     return ' ';
+//   }
+//   return returnString;
+// };
+
+//  console.log(decode('craft block argon meter bells brown croon droop'));
