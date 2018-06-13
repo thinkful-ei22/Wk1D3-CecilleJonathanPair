84e538aefa5f3c06c38fe2e73b6d47faac36a555
Cecille Salazar <cecillersalazar@gmail.com>
2018-06-13 12:01:16 -0700

    continuing drill 5

--- gistobjectdrills.js
+++ gistobjectdrills.js
@@ -38,23 +38,25 @@ Use console.log to show each property name and its associated value.
 // Store these objects in an array.
 // Iterate over the array and use console.log to show each person's job title and name.
 
-const firstObj = {
+const owner = {
   name: 'Jon',
   jobTitle: 'Superhero'
 };
 
-const secObj = {
+const emp1 = {
   name: 'Bob',
-  jobTitle: 'builder'
+  jobTitle: 'builder',
+  boss: 'Jon'
 };
 
-const thirdObj = {
+const emp2 = {
   name: 'Jane',
-  jobTitle: 'chef'
+  jobTitle: 'chef',
+  boss: 'Jon'
 };
 
-const myArray = [firstObj, secObj, thirdObj];
+const myArray = [owner, emp1, emp2];
 
-for (const key in myArray) {
-  console.log(myArray[key]);
+for (const prop in myArray) {
+  console.log(`${[key].name}`);
 }
